106d0479f8a148775d4aec07fc22379e254b9ecd
ZooZ.M <zeyadmostafa201@gmail.com>
2024-01-23 13:33:59 +0200

    refactor files

--- lib/features/Home/data/home_path.dart
+++ lib/features/Home/data/home_path.dart
@@ -11,9 +11,6 @@ const String table1 = 'assets/images/table.png';
 const String table2 = 'assets/images/table2.png';
 const String p1 = 'assets/images/1.png';
 const String p2 = 'assets/images/2.png';
-const String p3 = 'assets/images/3.png';
-const String p4 = 'assets/images/4.png';
-const String p5 = 'assets/images/5.png';
 const String p6 = 'assets/images/6.png';
 
 List<CategoryModel> categoryBlack = const [
@@ -28,11 +25,7 @@ List<CategoryModel> categoryWhite = const [
   CategoryModel(name: 'bed', path: bed1),
   CategoryModel(name: 'table', path: table1)
 ];
-List<String> onBoardList = const [
-  p3,
-  p4,
-  p5,
-];
+
 List<ItemModel> poplist = [
   ItemModel(
     name: 'Boogly Vhair',
